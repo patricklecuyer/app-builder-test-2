07892dc869a5eadadb8c38e9f1c361e6e54845c9
Patrick Lecuyer <patl@patl.io>
2026-08-13 10:49:21 -0400

    Initial commit.

---
+++ frontend/constants.ts
@@ -0,0 +1,3 @@
+export const AGENT_ID = 'projects/101364583064/locations/us-central1/reasoningEngines/826888269424754688';
+export const LOCATION = 'us-central1';
+export const BASE_URL = `https://${LOCATION}-aiplatform.googleapis.com/v1/${AGENT_ID}`;
